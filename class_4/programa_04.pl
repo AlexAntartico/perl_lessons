b961d6b116444fa35a913aad8132128d26dbbb56
konejin <konejin@gmail.com>
2022-03-24 23:20:05 -0600

    arrays 2

--- class_4/programa_04.pl
+++ class_4/programa_04.pl
@@ -26,5 +26,34 @@ while ($j < $nElementos){
     $j++;
 }
 
+print "\nusando un foreach\n\n";
+my $k = 0;
+my @nom_app = ();
+
+print "para poder imprimir uno por uno, es necesario acudir a un ciclo, aqui usando foreach\n";
+
+foreach (@nombres) {
+    push(@nom_app, "$nombres[$k] $apellidos[$k]");
+    print "$nom_app[$k]\n";
+    $k++;
+}
+
+=for comment
+foreach will iterate "n" times whatever element you put as parameter, 
+this means it will do whateve is between {} "n" number or times
+foreach (@nombres) {
+
+then we are pushing a combination of names array and apellidos array to the empty array
+@nom_app, k is a counter, starts with 0 and will iterate the number of times specified in 
+the foreach parameter between ()
+    push(@nom_app, "$nombres[$k] $apellidos[$k]");
+    
+    we are printing the individual values of the array
+    print "$nom_app[$k]\n";
+
+    autoincreasing k count...
+    $k++;
+}
+=cut
 
 exit
